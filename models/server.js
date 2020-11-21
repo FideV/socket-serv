ee343010b9416ce86d2a574ea15ccbb041f0ffc0
Fidelmar Vaca <v_a_f_10@hotmail.com>
2020-11-20 20:37:30 -0600

    Segundo commit

--- models/server.js
+++ models/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const http = require('http');
+const express  = require('express');
+const http     = require('http');
 const socketio = require('socket.io');
-const path = require('path');
+const path     = require('path');
+const cors     = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -22,6 +23,9 @@ class Server {
   middlewares(){
     //Desplegar el directorio publico
     this.app.use( express.static( path.resolve( __dirname, '../public')));
+
+    // CORS
+    this.app.use( cors() );
   }
 
   configurarSockets(){
